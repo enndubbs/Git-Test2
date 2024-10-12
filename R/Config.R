77be6477857c93881a8756a646c061369e35f1d1
Enndubbs <noahwright@gmail.com>
2024-10-12 11:30:21 -0500

    test

--- R/Config.R
+++ R/Config.R
@@ -1,14 +1,15 @@
 library(tidyverse)
 library(usethis)
 library(gitcreds)
+library(credentials)
 
 use_git_config(user.name = "Enndubbs", user.email = "noahwright@gmail.com")
 use_git()
 
 create_github_token()
-gitcreds_set()
-
+git_credential_ask
 use_github()
+  
+check_for_git()
 
-
-https://walker-data.com/posts/mapgl-dots/
+gitcreds_set_new("https://github.com")
